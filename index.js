a1c95e942596ca4b6b3b371229f5b64128d49a26
srueda99 <ruedamarinsebastian@gmail.com>
2022-04-01 19:38:38 -0500

    Upload to S3

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 // ---- IMPORTS ----
+
 const express = require('express');
 const AWS = require('aws-sdk');
 const dotenv = require('dotenv');
 const multer = require('multer');
 const mime = require('mime-types');
 const cors = require('cors');
-
-// ---- DECLARE THE CREDENTIALS FOR AWS ----
 dotenv.config();
+
+// ---- CREDENTIALS ----
+
 const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 const BKTMG_SECRET_KEY = process.env.BKTMG_SECRET_KEY;
+const BUCKET_NAME = process.env.BUCKET_NAME;
 
 // ---- START THE SERVER RUNNING ON PORT 2020 ----
+
 const app = express();
 const port = process.env.PORT || 2020;
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 });
 
-// ---- LOAD S3 SERVICE AND CONNECT TO AWS CLI USING THE BUCKET MANAGEMENT ----
+// ---- RENDER THE MAIN PAGE AND LOAD THE STYLES ----
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname +"/public/css"));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname +"/public/views/index.html");
+});
+
+// ---- LOAD S3 SERVICE AND CONNECT TO AWS CLI USING THE BUCKET MANAGEMENT USER ----
+
 AWS.config.update({region: 'us-east-2'});
 const s3 = new AWS.S3({
     accessKeyId: AWS_ACCESS_KEY,
     secretAccessKey: BKTMG_SECRET_KEY
 });
 
-// ---- RENDER THE MAIN PAGE ----
-app.get('/', (req, res) => {
-    res.sendFile(__dirname +"/views/index.html");
-});
-
 // ---- LIST THE BUCKETS ----
+
 app.get('/list', (req, res) => {
     s3.listBuckets(function(err, data) {
         if (err) {
@@ -42,18 +51,36 @@ app.get('/list', (req, res) => {
 });
 
 // ---- UPLOAD FILES ----
+
 const storage = multer.diskStorage({
     destination: 'temp/',
     filename: function(req, file, cllbck){
         cllbck("", Date.now()+"."+mime.extension(file.mimetype));
     }
 });
-
 const upload = multer({
     storage: storage
 });
 
 app.post('/upload', upload.single('uploadF'), (req, res) => {
     console.log(req);
-    res.send("Archivo subido, espero que no sea un virus.")
+    res.send("Archivo subido, espero que no sea un virus.");
+    console.log('Archivo:')
+    console.log(upload.single('uploadF'));
 });
+
+// const fileUp = (file) => {
+//     const content = fs.reasFileSync(file);
+//     const params = {
+//         Bucket: BUCKET_NAME,
+//         Key: 'name',
+//         Body: content
+//     };
+
+//     s3.upload(params, function(err, data) {
+//         if(err) {
+//             throw err;
+//         }
+//         console.log('Succesfully uploaded to S3');
+//     });
+// };
